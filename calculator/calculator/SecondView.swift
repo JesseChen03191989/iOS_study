bccd9f59fdcb6f45a62bd4684555eb66edbf475f
Jesse Chen <48510049@qq.com>
2018-05-11 15:48:02 +0800

    lession1
    lession1

---
+++ calculator/calculator/SecondView.swift
@@ -0,0 +1,19 @@
+//
+//  SecondView.swift
+//  calculator
+//
+//  Created by Jesse on 2018/5/11.
+//  Copyright © 2018 Jesse. All rights reserved.
+//
+
+import UIKit
+
+class SecondView: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
